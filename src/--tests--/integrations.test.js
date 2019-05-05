1eee2e23343dfe2c177205328a07ca20eb0419d1
Bijay Gyawali <bgwbc@mail.umkc.edu>
2019-05-05 15:44:45 -0500

    Add files via upload

---
+++ src/--tests--/integrations.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import Root from 'root';
+import App from 'components/App';
+import moxios from 'moxios';
+
+beforeEach(()=>{
+    moxios.install();
+    moxios.stubRequest('http://jsonplaceholder.typicode.com/comments', {
+        status: 200,
+        response: [{name: 'Fetched #1'},
+        {name: 'Fetched #2'},]
+    });
+});
+
+afterEach(()=>{
+    moxios.uninstall();
+});
+
+it('can fetch a list of comp and display them',(done)=>{
+    const wrapped=mount(
+        <Root>
+        <App />
+        </Root>
+    );
+    wrapped.find('.fetch-comments').simulate('click');
+    //setTimeout(()=>{
+        moxios.wait(()=>{
+        wrapped.update();
+        expect(wrapped.find('li').length).toEqual(2);
+        done();
+        wrapped.unmount();
+    }, 100);    
+});
